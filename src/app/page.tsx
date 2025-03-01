f05e549fd7d69db65367a66106e0fb70d6a2aa1f
Ricky Landino <ricky@landino.net>
2025-03-01 08:14:18 -0500

    disable eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,14 @@
 'use client'
 
-import { useState } from 'react'
 import MissionStatement from "@/components/MissionStatement";
-import { Dialog, DialogPanel } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Hero from '@/components/Hero';
 
-const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
-  { name: 'Log in', href: '#' },
-]
-
 export default function Home() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
-  return (
+    return (
         <>
-        <Hero />
-      <MissionStatement />
-      </>
-  )
+            <Hero />
+            <MissionStatement />
+        </>
+    )
 }
--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -55,7 +55,7 @@ export default function Resources() {
                 <div className="mt-6 border-t border-gray-100">
                     <dl className="divide-y divide-gray-100">
                         {resources.map((resource, idx) => (
-                            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0" key={idx}>
                                 <dt className="text-sm/6 font-medium text-gray-900">{resource.name}</dt>
                                 <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
                                     <ul>
